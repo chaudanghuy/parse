66c9b165eaf30ea2f99c6056e4a08d943e19639f
chaudanghuy <chaudanghuy@gmail.com>
2015-02-07 23:44:41 -0800

    add more

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("ZdZB07zZtmf6BWRJjQlxjNWW7zs1cikGVZE6Opc9", "uJF6xLuo5IMvJVLgYJt2oVkRnEQtDeFHhnxKAt6N");
+ 
+});
